b12c1b917afc4b45337f72e8a623c73334962e9f
Dan1Nguyen01 <98774378+Dan1Nguyen01@users.noreply.github.com>
2023-07-28 00:26:28 -0600

    debug 1 more time

--- airbnb_clone/src/pages/ProfilePage.jsx
+++ airbnb_clone/src/pages/ProfilePage.jsx
@@ -36,7 +36,7 @@ const ProfilePage = () => {
       <AccountNav />
       {subpage === "profile" && (
         <div className="text-center max-w-lg mx-auto">
-          Logged in as {user.name} ({user.email})
+          Logged in as {user?.name} ({user?.email})
           <button className="primary max-w-sm mt-2" onClick={logout}>
             Logout
           </button>
